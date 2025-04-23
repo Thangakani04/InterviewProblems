140b6b5d6162e31796f6882ed1f36082d141356c
Thangakani04 <thangakani1113@gmail.com>
2025-04-23 16:50:13 +0530

    Adding stream API problems

--- Streamproblems1.java
+++ Streamproblems1.java
@@ -1,8 +1,10 @@
 package InterviewProblems;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -13,6 +15,76 @@ public class Streamproblems1 {
 		partitionEvenOdd();
 		Streamproblems1 prime = new Streamproblems1();
 		prime.primenumbers();
+		sortByVowelCount();
+		FlattenNestedList();
+		groupAnagrams();
+		JoinStrings();
+		SumofNumbers();
+		RemoveEmptyStrings();
+		RemoveNulls();
+		
+	}
+
+	public static void RemoveNulls() {
+		List<String> list = Arrays.asList("java", null, "python", null, "c++");
+		List<String> notNullList = list.stream().filter(Objects::nonNull).collect(Collectors.toList());
+		System.out.println("Non null list " + notNullList);		
+	}
+
+	public static void RemoveEmptyStrings() {
+		 List<String> list = Arrays.asList("java", "", "python", "", "c++");
+		 List<String> nonEmpty = list.stream().filter(s-> !s.isEmpty()).collect(Collectors.toList());
+		 System.out.println("Non empty list " + nonEmpty);
+		
+	}
+
+	public static void SumofNumbers() {
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+		int sum = numbers.stream().mapToInt(n-> n.intValue()).sum();
+		int sum1 = numbers.stream().mapToInt(Integer::intValue).sum();
+		System.out.println("Sum of numbers : " + sum);
+		System.out.println("Sum of numbers : " + sum1);
+		
+	}
+
+	public static void JoinStrings() {
+		List<String> list = Arrays.asList("apple", "banana", "orange");
+		String joined = String.join(",", list);
+		System.out.println("String after joined with comma : " + joined);
+		
+	}
+
+	public static void groupAnagrams() {
+		
+		List<String> words = Arrays.asList("listen", "silent", "enlist", "rat", "tar", "art", "hello");
+		Map<String, List<String>> output = words.stream().collect(Collectors.groupingBy(word -> {
+			char[] chars = word.toCharArray();
+			Arrays.sort(chars);
+			return new String(chars);
+		}));
+		System.out.println(output);		
+	}
+
+	public static void FlattenNestedList() {
+		List<List<Integer>> nestedList = Arrays.asList(Arrays.asList(1,2),
+				Arrays.asList(3,4),
+				Arrays.asList(5,6));
+		List<Integer> output = nestedList.stream().flatMap(n-> n.stream()).collect(Collectors.toList());
+		List<Integer> output1 = nestedList.stream().flatMap(List::stream).collect(Collectors.toList());
+		System.out.println("Flatten nested list:" + output);
+		System.out.println("Flatten nested list:" + output1);
+		
+		
+	}
+
+	public static void sortByVowelCount() {
+		List<String> list = Arrays.asList("banana", "apple", "pear", "kiwi");
+		List<String> sorted = list.stream().sorted(Comparator.comparingLong(s-> s.chars()
+				.filter(c-> "aeiouAEIOU".indexOf(c) !=-1).count()))
+				.collect(Collectors.toList());
+		
+
+		System.out.println("Sorted strings by vowels count" + sorted);
 	}
 
 	private  void primenumbers() {
@@ -53,10 +125,7 @@ public class Streamproblems1 {
 		Map<Long, List<String>> groupFrequency = frequency
 												.entrySet().stream()
 												.collect(Collectors.groupingBy(Map.Entry::getValue,Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
-		System.out.println("Group elements by Frequency : "+groupFrequency);
-												
-		
-		
+		System.out.println("Group elements by Frequency : "+groupFrequency);												
 	}
 
 }
