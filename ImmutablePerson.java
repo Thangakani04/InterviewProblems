4a7d1443faac30a0052f71f11ed74e2391c78ba8
Thangakani04 <thangakani1113@gmail.com>
2025-05-03 18:14:46 +0530

    Immutable class implemented

---
+++ ImmutablePerson.java
@@ -0,0 +1,24 @@
+package InterviewProblems;
+
+public final class ImmutablePerson {
+	
+	private final String name;
+	private final int age;
+	
+	public ImmutablePerson(String name , int age) {
+		this.name = name;
+		this.age =age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	
+
+}
+
+
