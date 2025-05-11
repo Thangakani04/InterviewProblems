87d22dbf75058ec6728b9a03aed7ac5f1af20e31
Thangakani04 <thangakani1113@gmail.com>
2025-05-11 18:55:57 +0530

    Stream problems solved

--- HandsonPreparation/HandsonProblems.java
+++ HandsonPreparation/HandsonProblems.java
@@ -29,11 +29,60 @@ public class HandsonProblems {
 	//Find the second highest number in a list of integers.
 	public static void main(String[] args) {
 		
-		secongHighestNumber();
-		ReturnStringwithAandLength();
-		groupEmployeesByDepartment();
-		duplicateNumbers();
+		secongHighestNumber(); 			//1
+		ReturnStringwithAandLength();	//2
+		groupEmployeesByDepartment();	//3
+		duplicateNumbers();				//4
+		groupwordByFrequency();			//5
+		groupwordsByFrequency();		//6
+		sortListofStrings();			//7
+		sortListofStrLenAlphebetical();	//8
 		
+		
+	}
+
+	public static void sortListofStrLenAlphebetical() {
+		//Sort a list of strings by their length and then alphabetically.
+		List<String> names = Arrays.asList("Kani" ,"Alex", "Anu" ,"Swetha" ,"Nivedha" ,"Thangakani");
+		List<String> sortedList = names.stream().sorted((s1,s2) -> {
+			int len = Integer.compare(s1.length(), s2.length());
+			if( len == 0) {
+				return s1.compareTo(s2);
+			}
+			return len;
+		}).collect(Collectors.toList());
+		System.out.println("Sorted list of strings by their length and then alphabetically :"+ sortedList);
+		
+	}
+
+	public static void sortListofStrings() {
+		//Sort a list of strings by their length
+		List<String> names = Arrays.asList("Kani" ,"Alex", "Anu" ,"Swetha" ,"Nivedha" ,"Thangakani");
+		List<String> sortedList = names.stream().sorted((s1,s2) -> Integer.compare(s1.length(), s2.length()))
+				.collect(Collectors.toList());
+		System.out.println("Sorted list of strings by their length :"+ sortedList);
+				
+		
+	}
+
+	public static void groupwordsByFrequency() {
+		//Given a list of words, count the frequency of each character in all the words combined.
+		
+		List<String> words =Arrays.asList("apple" ,"banana" ,"grape");
+		String combinedString = String.join("", words);
+		Map<Character,Long> output = combinedString.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+		System.out.println(output);
+		
+	}
+
+	public static void groupwordByFrequency() {
+		// Given a word, count the frequency of each character in the word.
+		String name = "Thangakani";
+		//char[] arr = name.toCharArray();
+		Map<Character,Long> output = name.chars()
+				.mapToObj(c -> (char)c)
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.println("The frequency of each character in the word : " + output);
 		
 	}
 
@@ -41,7 +90,9 @@ public class HandsonProblems {
 		//Find the duplicate elements in a list of integers.
 		List<Integer> numbers = Arrays.asList(1,2,4,4,5,2,7,8,9,5);
 		Set<Integer> duplicateNumbers = numbers.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-				.entrySet().stream().filter(e -> e.getValue() > 1)
+				.entrySet()
+				.stream()
+				.filter(e -> e.getValue() > 1)
 				.map(Map.Entry::getKey).collect(Collectors.toSet());
 		System.out.println("Duplicates numbers are :" + duplicateNumbers);
 		
