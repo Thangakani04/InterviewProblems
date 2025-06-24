66fe791db79d74453afe69bfff55ddf39a88d641
Thangakani04 <thangakani1113@gmail.com>
2025-06-24 19:19:36 +0530

    Copy constructor problem solved

---
+++ Java/ExampleCopyConstructor.java
@@ -0,0 +1,19 @@
+package InterviewProblems;
+
+class InterviewBit1{
+	String name;
+	int age;
+	
+	InterviewBit1(InterviewBit1 bit){
+		this.name = bit.name;
+		this.age = bit.age;
+	}
+}
+
+public class ExampleCopyConstructor {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
