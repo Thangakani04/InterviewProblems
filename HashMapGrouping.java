2ecd5f4cc7d1f603816f301009a041164decb8f5
Thangakani04 <thangakani1113@gmail.com>
2025-06-25 19:32:30 +0530

    Grouping using hashmap

---
+++ HashMapGrouping.java
@@ -0,0 +1,21 @@
+package InterviewProblems;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class HashMapGrouping {
+	public static void main(String[] args) {
+		
+		List<String> words = Arrays.asList("apple" ,"banana" ,"apple" , "orange" ,"apple" ,"banana");
+		
+		HashMap<String, Integer> result = new HashMap<String, Integer>();
+		
+		for(String s: words) {
+		result.put(s, result.getOrDefault(s, 0)+1);
+		}
+		
+		System.out.println(result);
+	}
+
+}
