b89ca78f00d9c4480981761a63cbd93d8636a6f1
Thangakani04 <thangakani1113@gmail.com>
2025-06-26 20:46:43 +0530

    Stream problem solved using filtering and sorting

---
+++ StreamAPIInterviewPreparation/StreamSortingFiltering.java
@@ -0,0 +1,54 @@
+package StreamAPI;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+class EmpClass1 {
+        int id;
+        String name;
+        int age;
+        double salary;
+
+  
+        public EmpClass1(int id, String name, int age, double salary) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+        }
+
+    
+        public int getId() { return id; }
+        public String getName() { return name; }
+        public int getAge() { return age; }
+        public double getSalary() { return salary; }
+    }
+
+public class StreamSortingFiltering {
+	
+	public static void main(String[] args) {
+		List<EmpClass1> employees = Arrays.asList(
+			    new EmpClass1(1, "Alice", 28, 70000),
+			    new EmpClass1(2, "Bob", 35, 90000),
+			    new EmpClass1(3, "Charlie", 32, 120000),
+			    new EmpClass1(4, "David", 29, 85000),
+			    new EmpClass1(5, "Eva", 45, 110000),
+			    new EmpClass1(6, "Frank", 31, 105000),
+			    new EmpClass1(7, "Grace", 38, 95000)
+			);
+		
+		List<String> output = employees.stream()
+				.filter(emp -> emp.getAge() >= 30)
+				.sorted(Comparator.comparingDouble(EmpClass1::getSalary).reversed())
+				.map(emp -> emp.getName())
+				.limit(3)
+				.toList();
+		
+		System.out.println(output);
+				
+	}
+
+}
