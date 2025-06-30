7322fa040e38025f162b476e6734a92f4c8033d7
Thangakani04 <thangakani1113@gmail.com>
2025-06-30 20:57:51 +0530

    Posdemo added

---
+++ PosdemoApplication.java
@@ -0,0 +1,13 @@
+package com.postgresql.posdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosdemoApplication.class, args);
+	}
+
+}
