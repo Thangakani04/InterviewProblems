77ff60f3bbc0b102b5daca94f39e4fce85fcf49e
Thangakani04 <thangakani1113@gmail.com>
2025-07-27 20:38:52 +0530

    Pyramid pattern solved

---
+++ Pyramidpattern.java
@@ -0,0 +1,31 @@
+package InterviewProblems;
+
+public class Pyramidpattern {
+	
+	public static void main(String[] args) {
+		
+		int rows =4;
+		for(int i=0;i<rows;i++) {
+			for(int j =0;j<rows-i-1;j++) {
+				System.out.print("  ");
+			}
+			for(int k=0;k<2*i+1;k++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+		}
+		
+		for(int i=rows;i>0;i--) {
+			for(int j=0;j<rows-i;j++) {
+				System.out.print("  ");
+			}
+			for(int k= 0; k <2*i-1;k++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
